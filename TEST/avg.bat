dbfcdcd7da3041bd83a9b4a69a143b853b5fd198
ilein <ilein2005@inbox.ru>
2019-05-06 05:56:16 +0500

    Add files via upload

---
+++ TEST/avg.bat
@@ -0,0 +1,8 @@
+@echo off
+
+chcp 65001
+cls 
+cd ..
+php user_text_util.php countAverageLineCount semicolon
+
+pause >nul
---
+++ TEST/error.bat
@@ -0,0 +1,9 @@
+@echo off
+
+chcp 65001
+cls
+cd ..
+
+php user_text_util.php replaceDates comma
+
+pause >nul
