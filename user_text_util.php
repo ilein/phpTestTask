e366880d4b5b6a90c7752a86f1d737e9155f576a
ilein <ilein2005@inbox.ru>
2019-05-07 22:48:39 +0500

    Add files via upload

--- user_text_util.php
+++ user_text_util.php
@@ -1,10 +1,11 @@
 <?php
-include 'item.php';
 
-if ($argc!=3) 
-{ 
-	echo "Получено неверное количество аргументов\n";
-	return;
+use \ilein\Member;
+include_once 'item.php';
+
+if ($argc!=3) { 
+    echo "Получено неверное количество аргументов\n";
+    return;
 };
 
 // проверка аргументов
@@ -12,13 +13,13 @@ $arr_fnc = array("countAverageLineCount", "replaceDates");
 $arr_separator = array("comma"=>",", "semicolon"=>";");
 
 if (!in_array($argv[1], $arr_fnc)) {
-	echo "Функция не найдена\n";
-	return;
+    echo "Функция не найдена\n";
+    return;
 }
  
 if (!array_key_exists($argv[2], $arr_separator)) {
-	echo "Разделитель не найден\n";
-	return;
+    echo "Разделитель не найден\n";
+    return;
 }
 
 // разбор csv
@@ -26,52 +27,49 @@ if (!array_key_exists($argv[2], $arr_separator)) {
 $membersArray = array();
 
 $row = 1;
-ini_set('auto_detect_line_endings',TRUE);
+ini_set('auto_detect_line_endings',true);
 $handle = fopen('people.csv','r');
-while (($data = fgetcsv($handle, "", $arr_separator[$argv[2]]) ) !== FALSE ) {
-	$num = count($data);
-	$row++;
-	if ($num === 2)
-	{
-		$item = new Member($data[0], $data[1]);
-		array_push($membersArray, $item);
-	}
-	else 
-	{
-		echo "Найдена ошибка в файле .csv";
-		return;
-	}
+
+while (($data = fgetcsv($handle, "", $arr_separator[$argv[2]]) ) !== false ) {
+    $num = count($data);
+    $row++;
+    if ($num === 2) {
+        $item = new Member($data[0], $data[1]);
+        array_push($membersArray, $item);
+    }
+    else {
+        echo "Найдена ошибка в файле .csv";
+        return;
+    }
 }
-ini_set('auto_detect_line_endings',FALSE);
+ini_set('auto_detect_line_endings', false);
 
 // вызов функций
-if ($argv[1]==="countAverageLineCount")
-{
-	foreach ($membersArray as $item) {
-		$item->PrintAvg();
-	};
+if ($argv[1]==="countAverageLineCount") {
+    foreach ($membersArray as $item) {
+        $item->printAvg();
+    };
 };
 
-if ($argv[1]==="replaceDates")
-{
-	$dir = 'output_texts';
-	// наличие директории
-	if (!file_exists($dir)) {
-		mkdir($dir);
-	}
-	else {
-		//чистка
-		$files = glob($dir."/*");
-		foreach($files as $file){
-			if(is_file($file))
-				unlink($file);
-		}
-	};
-	
-	foreach ($membersArray as $item) {
-		$item->PrintReplaceCnt();
-	};
-	
+if ($argv[1]==="replaceDates") {
+    $dir = 'output_texts';
+    // наличие директории
+    if (!file_exists($dir)) {
+        mkdir($dir);
+    }
+    else {
+        //чистка
+        $files = glob($dir."/*");
+        foreach($files as $file){
+            if(is_file($file)) {
+                unlink($file);
+            }
+        }
+    };
+
+    foreach ($membersArray as $item) {
+        $item->printReplaceCnt();
+    };
 };
 
 ?>
